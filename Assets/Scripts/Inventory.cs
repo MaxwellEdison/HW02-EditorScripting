2f314e1889e6184773de204204d6d64056d039d8
Samuel <maxwell.moyya@gmail.com>
2020-10-17 02:12:33 -0500

    Progress on inventory/equipment editor

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class CurrentlyEquipped
+{
+    //public Equipment[] thing;
+}
+
+public class Inventory : MonoBehaviour
+{
+    public CurrentlyEquipped[] currentlyEquipped;
+}
---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rarity
+{
+    Common,
+    Uncommon,
+    Rare,
+    Legendary
+}
+
+public enum EquipSlot
+{
+    Head,
+    Torso,
+    Arms,
+    Legs,
+    Feet
+}
+
+public enum CurSlot
+{
+    Head,
+    Torso,
+    Arms,
+    Legs,
+    Feet,
+    Inventory
+}
+
+[Serializable]
+public class ItemProperties
+{
+    public string name;
+    public EquipSlot equippableSlot;
+    public int attack;
+    public int defense;
+    public Rarity rarity;
+    public int goldValue;
+    public CurSlot currentSlot;
+}
+public class ItemManager : MonoBehaviour
+{
+    public ItemProperties[] itemDefinition;
+}
